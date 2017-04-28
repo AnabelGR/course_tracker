221877ff4b81ab3dc262d3002b863bb678ea666d
Anabel Ramirez <anabel@apparentinc.com>
2017-04-28 11:54:23 -0700

    begins jquery branching functions.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,19 @@
 $(document).ready(function() {
-  var question1 = parseInt($("input:radio[name=question1]:checked").val());
-  var question2 = parseInt($("input:radio[name=question2]:checked").val());
-  var question3 = parseInt($("input:radio[name=question3]:checked").val());
-  var question4 = parseInt($("input:radio[name=question4]:checked").val());
-  var question5 = parseInt($("input:radio[name=question5]:checked").val());
-  var result = question1 + question2 + question3 + question4 + question5
+  $("#form-group").submit (function(event) {
+  var question1 = parseInt($("input:radio[name=answer1]:checked").val());
+  var question2 = parseInt($("input:radio[name=answer2]:checked").val());
+  var question3 = parseInt($("input:radio[name=answer3]:checked").val());
+  var question4 = parseInt($("input:radio[name=answer4]:checked").val());
+  var question5 = parseInt($("input:radio[name=answer5]:checked").val());
+  var resultFrontEnd = question1 === 1;
+  var resultBackEnd = question1 === 2;
 
+   if (resultFrontEnd) {
+   $(".option").hide();
+   $("#CSS").show();
+   }
 
-
-
+});
 
 event.preventDefault();
 });

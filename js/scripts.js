669892a678320a227e02d555cc9c4e5927f3f501
Anabel Ramirez <anabel@apparentinc.com>
2017-04-28 13:37:56 -0700

    show four answers determned by values selected.

--- js/scripts.js
+++ js/scripts.js
@@ -7,24 +7,36 @@ $(document).ready(function() {
   var question4 = parseInt($("input:radio[name=answer4]:checked").val());
   var question5 = parseInt($("input:radio[name=answer5]:checked").val());
   var resultQuestion1 = question1 === 1;
-  var resultQuestion1a = question1 === 10;
+  var resultQuestion11 = question1 === 10;
 
 
-  if (resultQuestion1 + question2 === 2) {
+  if (resultQuestion1 + question2 <=2) {
     $(".option").hide();
     $("#CSS").show();
   }
-  else if (resultQuestion1 + question2 === 3) {
+  else if (resultQuestion1 + question2 <=3) {
     $(".option").hide();
     $("#webDesign").show();
   }
-  // else if (resultQuestion1a + question3 === 15) {
-  //   $(".option").hide();
-  //   $("#cSharp").show();
+  else if (resultQuestion11 + question3 <=15) {
+    $(".option").hide();
+    $("#cSharp").show();
   // }
-  // else if (resultQuestion1a + question5 === 20) {
+  // else if (resultQuestion11 + question3 <=20) {
   //   $(".option").hide();
   //   $("#Java").show();
   // }
+  // else if (resultQuestion11 + question3 <=25) {
+  //   $(".option").hide();
+  //   $("#PHP").show();
+  // }
+  // else if (resultQuestion11 + question3 <=30) {
+  //   $(".option").hide();
+  //   $("#Ruby").show();
+  }
+  else {
+    $(".option").hide();
+    $("#pleasechoose").show();
+  }
   });
 });

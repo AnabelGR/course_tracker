2a5a8c54f5a059b704d3b5f6f8845cb4253ec41d
Anabel Ramirez <anabel@apparentinc.com>
2017-04-28 12:18:02 -0700

    show one answer determined by value.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,21 @@
 $(document).ready(function() {
-  $("#form-group").submit (function(event) {
+  $(".formOne").submit (function(event) {
+  event.preventDefault();
   var question1 = parseInt($("input:radio[name=answer1]:checked").val());
   var question2 = parseInt($("input:radio[name=answer2]:checked").val());
   var question3 = parseInt($("input:radio[name=answer3]:checked").val());
   var question4 = parseInt($("input:radio[name=answer4]:checked").val());
   var question5 = parseInt($("input:radio[name=answer5]:checked").val());
-  var resultFrontEnd = question1 === 1;
-  var resultBackEnd = question1 === 2;
+  var resultQuestion1 = question1 === 1;
 
-   if (resultFrontEnd) {
-   $(".option").hide();
-   $("#CSS").show();
+
+   if (resultQuestion1 + question2 === 2) {
+     $(".option").hide();
+     $("#CSS").show();
    }
-
-});
-
-event.preventDefault();
+  //  else if (resultQuestion1 + question2 === 3) {
+  //     $(".option").hide();
+  //     $("#webDesign").show();
+  //   }
+  });
 });

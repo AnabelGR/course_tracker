9b300c2b19893ea5ea02d1f8c6aca08a9b81432e
Anabel Ramirez <anabel@apparentinc.com>
2017-04-28 10:36:46 -0700

    edits html values to create numbers for js branching function.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,13 @@
 $(document).ready(function() {
-  var question1 = $("input:radio[name=question1]:checked").val();
-  var question2 = $("input:radio[name=question2]:checked").val();
-  var question3 = $("input:radio[name=question3]:checked").val();
-  var question4 = $("input:radio[name=question4]:checked").val();
-  var question5 = $("input:radio[name=question5]:checked").val();
+  var question1 = parseInt($("input:radio[name=question1]:checked").val());
+  var question2 = parseInt($("input:radio[name=question2]:checked").val());
+  var question3 = parseInt($("input:radio[name=question3]:checked").val());
+  var question4 = parseInt($("input:radio[name=question4]:checked").val());
+  var question5 = parseInt($("input:radio[name=question5]:checked").val());
+  var result = question1 + question2 + question3 + question4 + question5
+
+
+
 
 
 event.preventDefault();
